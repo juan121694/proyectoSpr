d7f8677c3884736b33024e2451af529a8e2d1ce4
juan <juan@debian>
2018-11-16 14:31:31 -0600

    [Hasta leccion 53]

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -5,14 +5,20 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.services.IClienteService;
 
@@ -24,9 +30,13 @@ public class ClienteController {
 	private IClienteService clienteService;
 	
 	@RequestMapping(value="/listar", method=RequestMethod.GET)
-	public String listar(Model model){
+	public String listar(@RequestParam(name="page",defaultValue="0") int page,Model model){
+		
+		Pageable pageRequest = PageRequest.of(page, 4);
+		
+		Page<Cliente> clientes=clienteService.findAll(pageRequest);
 		model.addAttribute("titulo","Listado de clientes");
-		model.addAttribute("clientes",clienteService.findAll());
+		model.addAttribute("clientes",clientes);
 		return "listar";
 	}
 	
@@ -40,12 +50,17 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		Cliente cliente = null;
 		
 		if(id>0) {
 			cliente = clienteService.findOne(id);
+			if(cliente==null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD!");
+				return "redirect:/listar";
+			}
 		}else {
+			flash.addFlashAttribute("error", "El ID del cliente no puede ser cero!");
 			return "redirect:/listar";
 		}
 		
@@ -55,22 +70,27 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result,Model model,SessionStatus status){
+	public String guardar(@Valid Cliente cliente, BindingResult result,Model model,RedirectAttributes flash,SessionStatus status){
 		
 		if(result.hasErrors()) {
 			model.addAttribute("titulo","Formulario de Cliente");
 			return "form";
 		}
+		String mensajeflash=(cliente.getId() != null)? "Cliente editado con exito!" : "Cliente creado con exito!";
+		
 		clienteService.save(cliente);
 		status.setComplete();
+		flash.addFlashAttribute("success", mensajeflash);
 		return "redirect:listar";
 	}
 	
 	@RequestMapping(value="/eliminar/{id}")
-	public String eliminar(@PathVariable(value="id") Long id) {
+	public String eliminar(@PathVariable(value="id") Long id, RedirectAttributes flash) {
 		
 		if(id>0) {
 			clienteService.delete(id);
+			flash.addFlashAttribute("success", "Cliente eliminado con exito");
+
 		}
 		
 		return "redirect:/listar";
